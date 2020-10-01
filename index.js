b5f0eec5048a0982b54b674a96eb6a5840663437
Brandon Scott <btfs95@gmail.com>
2020-09-30 20:15:18 -0700

    running server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
-const app = express()
+const app = express();
 
 app.listen(3000, () => {
-    console.log('server started listening on PORT http://localhost:3001')
-})
+  console.log('server started listening on PORT http://localhost:3001');
+});
